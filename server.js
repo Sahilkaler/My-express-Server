225024a05c7e93aa9b381e3a2ae8a0a2b4e22015
sahil-kaler <119391867+sahil-kaler@users.noreply.github.com>
2023-08-29 20:35:03 +0530

    commit

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 const express = require("express");
 const app = express();
-app.listen(3000);
+
+app.get("/",function(request,response){
+    response.send("hello");
+});
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
