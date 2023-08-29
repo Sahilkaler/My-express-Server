1b88cf4d6924515771039a74ff6a027df1cc3474
sahil-kaler <119391867+sahil-kaler@users.noreply.github.com>
2023-08-29 21:16:27 +0530

    commit

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.get("/",function(request,response){
-    response.send("hello");
+app.get("/",function(req,res){
+    res.send("hello");
 });
 
+app.get("/contact",function(req,res){
+    res.send("<h1>Contact me at: sahilkaler12344@gmail.com</h1>");
+});
+app.get("/about",function(req,res){
+    res.send("<p>Hello my name is Sahil Kaler. I'm a freshman in Computer Science and Engineering.I have keen intersets in Tech and i like to learn new things.</p>");
+});
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
